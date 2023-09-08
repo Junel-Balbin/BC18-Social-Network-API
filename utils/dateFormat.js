4833e73a57c20ecab0a0ec412d11dc4ab6a315d0
Junel <robin.lennuj@gmail.com>
2023-09-07 18:40:09 -0700

    Added Timestamp format & 12 hour format.

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -20,3 +20,48 @@ const addDateSuffix = date => {
       return `${dateStr}th`;
   }
 };
+
+// Format timestamp.
+module.exports = (timestamp, { monthLength = 'short', dateSuffix = true } = {}) => {
+  const monthsShort = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'];
+  const monthsLong = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'];
+
+  const dateObj = new Date(timestamp);
+  const formattedMonth = monthLength === 'short' ? monthsShort[dateObj.getMonth()] : monthsLong[dateObj.getMonth()];
+  
+  const dayOfMonth = dateSuffix ? addDateSuffix(dateObj.getDate()) : dateObj.getDate();
+  const year = dateObj.getFullYear();
+  
+  // 12 hour format.
+  let hour = dateObj.getHours() % 12 || 12; 
+  const minutes = dateObj.getMinutes();
+  const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+
+  const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes.toString().padStart(2, '0')} ${periodOfDay}`;
+
+  return formattedTimeStamp;
+};
