755e3c5e37da271044c4bd66397896758e9a4b03
Junel <robin.lennuj@gmail.com>
2023-09-09 20:46:52 -0700

    Worked on multiple index.js.

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Importing User, Thought and Reaction models.
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
+
+// Exporting the models.
+module.exports = { User, Thought, Reaction };
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// Importing the Express router.
+const router = require('express').Router();
+
+// Importing the API routes from the 'api' folder.
+const apiRoutes = require('./api');
+
+// Using the '/api' route for the imported API routes
+router.use('/api', apiRoutes);
+
+// Error message when handling request with wrong routes.
+router.use((req, res) => res.send('Error Wrong Route 😝.'));
+
+// Exporting the router.
+module.exports = router;
