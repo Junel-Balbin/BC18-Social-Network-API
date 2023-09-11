01c4e9e4112a9fad00c5d1eddb72280c1b6baa04
Junel <robin.lennuj@gmail.com>
2023-09-10 20:34:43 -0700

    Building the Controllers.js & added connection.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,16 +1,31 @@
-const { Thought, User, Reaction } = require("../models");
+const { Thought, User } = require("../models");
 
 const thoughtController = {
 
     // GET all thoughts.
+    async getAllThought(req, res) {
+
+    },
 
     // GET thought by ID.
+    async getThoughtById(req, res) {
+
+    },
 
     // Add thought.
+    async addThought(req, res) {
+
+    },
 
     // Update thought.
+    async updateThought(req, res) {
+
+    },
 
     // Delete thought.
+    async deleteThought(req, res) {
+        
+    }
 
 };
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,42 @@
-const { Thought, User, Reaction } = require("../models");
+const { Thought, User } = require("../models");
 
 
 const userController = {
 
     // Todos: GET all users.
+    async getUsers(req, res) {
+
+    },
 
     // Todos: GET user by ID.
+    async getUserById(req, res) {
+
+    },
 
     // Todos: Add user.
+    async addUser(req, res) {
+
+    },
 
     // Todos: Update user.
+    async updateUser(req, res) {
+
+    },
 
     // Todos: Delete user.
+    async deleteUser(req, res) {
+
+    },
 
     // Todos: Add friend.
+    async addFriend(req, res) {
+
+    },
 
     // Todos: Delete friend.
+    async deleteFriend(req, res) {
+
+    }
 
 };
 
